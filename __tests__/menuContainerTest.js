1894d970c3bcf264c7b30ba692d3ccc622161847
Diane Moon <dianejmoon@gmail.com>
2023-07-05 19:30:41 -0400

    menuContainerTest

--- __tests__/menuContainerTest.js
+++ __tests__/menuContainerTest.js
@@ -1,5 +1,20 @@
-import React from 'React';
-import userEvent from '@testing-library/user-event'
+const server = 'http://localhost:3000';
+const request = require('supertest');
+const fs = require('fs');
+import React from 'react';
+import App from '../Client/App.jsx';
+import { Store } from '../Client/store.js';
+import { Provider } from 'react-redux';
 import { render, screen, waitFor } from '@testing-library/react';
-import regeneratorRuntime from 'regenerator-runtime';
 
+describe('test for menuContainer.jsx', () => {
+  // beforeAll(() => {
+  //   const renderedMenuContainer = render(<menuContainer/>);
+  // })
+  test('menuContainer.js renders on the page', () => {
+    const tree = renderer
+      .create(<menuContainer/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
