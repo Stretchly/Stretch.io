74eeacda3b3479e9ca4de481f10c44952c217eda
jasony000 <jasony1004@gmail.com>
2023-07-05 11:25:27 -0400

    authUser passed tests

--- Server/controller/UserController.js
+++ Server/controller/UserController.js
@@ -10,7 +10,7 @@ userController.registerUser = asyncHandler(async (req, res, next) => {
 
   //if (User.find({ username })) throw new Error("User already exists");
   const userCheck = await User.find({ username });
-  console.log('username : ', username, ' : ', userCheck);
+  //console.log('username : ', username, ' : ', userCheck);
   if (userCheck.length > 0) throw new Error('User already exists');
   // if we find the user we need to throw an error
   res.locals.registeredUser = await User.create({
@@ -25,20 +25,25 @@ userController.authUser = async (req, res, next) => {
   const { username, password } = req.body;
   // get user from db
   const userCheck = await User.find({ username });
-  //console.log('username : ', username, ' : ', userCheck);
+  console.log('username : ', username, ' : ', userCheck);
   // check if user is in db
   if (userCheck.length <= 0) {
     return next({ message: { err: 'user not found' } });
   }
   //compare password
-  bcrypt.compare(password, userCheck.password, function (err, result) {
-    if (result === true) {
-      res.locals.user = userCheck[0];
-      return next();
-    } else {
-      return next({ message: { err: 'user not found or wrong password' } });
+  const match = bcrypt.compare(
+    password,
+    userCheck[0].password,
+    function (err, result) {
+      if (result === true) {
+        //console.log('this is the log ', userCheck[0]);
+        res.locals.user = userCheck[0];
+        return next();
+      } else {
+        return next({ message: { err: 'user not found or wrong password' } });
+      }
     }
-  });
+  );
 };
 
 // Delete a user
@@ -46,7 +51,7 @@ userController.authUser = async (req, res, next) => {
 
 userController.deleteUser = async (req, res, next) => {
   const { username, password } = req.body;
-  console.log('entering deletion middleware ');
+  //console.log('entering deletion middleware ');
 
   try {
     // find the userand delete
