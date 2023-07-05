b3b07c4c1075185b4e78ec0e7526c287d5f52322
Eivind Del Fierro <Eivind.Del.Fierro@gmail.com>
2023-07-05 13:46:52 -0400

    updated header files

---
+++ Client/containers/HeaderContainer.jsx
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module Header
+ * @author Eivind Del Fierro, Morah Geist
+ * @date 07/2023
+ * @description header feature on main page of app
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import * as actions from '../actionCreator/actionCreator.js';
+
+const HeaderContainer = () => {
+    return (
+        <div className="appHeaderBox">
+        <h4 className="mainHeader">Stretch</h4>
+        <button>Logout</button>
+        </div>
+    );
+}
+
+export default HeaderContainer;
